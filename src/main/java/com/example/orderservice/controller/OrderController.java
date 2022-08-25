86ab7936e3c7b5089df96ddece2013ccfbf7d46f
kakao <kakao1302@naver.com>
2022-08-25 17:57:53 +0900

    Initial commit

---
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,94 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
+import com.example.orderservice.service.OrderService;
+import com.example.orderservice.vo.RequestOrder;
+import com.example.orderservice.vo.ResponseOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order-service")
+@Slf4j
+public class OrderController {
+
+    private OrderService orderService;
+    private Environment env;
+    private KafkaProducer kafkaProducer;
+    private OrderProducer orderProducer;
+    @Autowired
+    public OrderController(OrderService orderService, Environment env, KafkaProducer kafkaProducer, OrderProducer orderProducer) {
+        this.orderService = orderService;
+        this.env = env;
+        this.kafkaProducer = kafkaProducer;
+        this.orderProducer = orderProducer;
+    }
+
+    @GetMapping("/health_check")
+    public String check(){
+        return String.format("It's Working Order Service on Port %s", env.getProperty("local.server.port"));
+    }
+
+    @PostMapping("/{userId}/orders")
+    public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId, @RequestBody RequestOrder order){
+        log.info("Before add orders data");
+        ModelMapper mapper = new ModelMapper();
+
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        OrderDto orderDto = mapper.map(order, OrderDto.class);
+
+        orderDto.setUserId(userId);
+        //jpa
+        OrderDto createdOrder = orderService.createOrder(orderDto);
+        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+        //kafka
+//        orderDto.setOrderId(UUID.randomUUID().toString());
+//        orderDto.setTotalPrice(order.getQty() * order.getUnitPrice());
+
+        kafkaProducer.send("example-catalog-topic", orderDto);
+//        orderProducer.send("orders", orderDto);
+
+//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+
+
+        log.info("After added orders data");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<List<ResponseOrder>> getOrder(@PathVariable("userId") String userId) throws Exception{
+        log.info("Before retrieve orders data");
+        Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
+
+        List<ResponseOrder> result = new ArrayList<>();
+
+        orderList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseOrder.class));
+        });
+//        try{
+//            Thread.sleep(3000);
+//            throw new Exception("장애 발생");
+//        } catch (InterruptedException ex){
+//            log.warn(ex.getMessage());
+//        }
+
+        log.info("After retrieve orders data");
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
---
+++ src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    private OrderRepository repository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDto) {
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getQty() * orderDto.getUnitPrice());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderEntity orderEntity = mapper.map(orderDto, OrderEntity.class);
+
+        repository.save(orderEntity);
+
+        OrderDto resultOrderDto = mapper.map(orderEntity, OrderDto.class);
+
+        return resultOrderDto;
+    }
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        OrderEntity orderEntity = repository.findByOrderId(orderId);
+        OrderDto orderDto = new ModelMapper().map(orderEntity, OrderDto.class);
+
+        return orderDto;
+    }
+
+    @Override
+    public Iterable<OrderEntity> getOrdersByUserId(String userId) {
+        return repository.findByUserId(userId);
+    }
+}
